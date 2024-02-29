f04afbe46b0f75ea6b799fd95770e69657cbe68c
Кот Анастасия <hugo.greendal@gmail.com>
2024-02-29 20:57:43 +0300

    [WIP] first version: email sending

---
+++ MailSender.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using MailKit.Net.Smtp;
+using MailKit;
+using MimeKit;
+//github test
+namespace MailSender;
+
+public class EmailService
+{
+	const string senderEmail = "tester.bob.1468@gmail.com";
+	const string senderPassword = "gjdi qjsn zauc bjnd";
+
+	public EmailService() { }
+
+	public void SendEmail(string receiverEmail, string subject,
+	 					  string emailText)
+	{
+		MimeMessage emailMessage = new MimeMessage();
+
+		emailMessage.From.Add(new MailboxAddress("Sender", senderEmail));
+		emailMessage.To.Add(MailboxAddress.Parse(receiverEmail));
+		emailMessage.Subject = subject;
+		emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+		{
+			Text = emailText
+		};
+
+		using (var client = new SmtpClient())
+		{
+			string smtpServer = "smtp.gmail.com";
+			int serverPort = 465;
+			bool sslEnabled = true;
+
+			try
+			{
+				client.Connect(smtpServer, serverPort, sslEnabled);
+				client.Authenticate(senderEmail, senderPassword);
+				var serverMessage = client.Send(emailMessage);
+				Console.WriteLine(serverMessage);
+			}
+			catch (Exception e)
+			{
+				Console.Write(e.Message);
+			}
+			finally
+			{
+				client.Disconnect(true);
+			}
+		}
+	}
+}
+
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+
+namespace MailSender;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Send();
+    }
+
+    public static void Send()
+    {
+        Console.Write("Enter receiver's email: ");
+        string receiverEmail = Console.ReadLine();
+
+        Console.Write("Enter the subject of the mail: ");
+        string subject = Console.ReadLine();
+
+        string pathToHtmlFrom = "/Users/greendal/Projects/MailSender/Form.html";
+        string text = File.ReadAllText(pathToHtmlFrom);
+
+        /*Console.Write("Enter text of the mail: ");
+        string text = Console.ReadLine();*/
+
+        EmailService emailService = new EmailService();
+        emailService.SendEmail(receiverEmail, subject, text);
+    }
+}
+
