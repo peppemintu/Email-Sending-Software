c0ec3f89c78f717468dccfa91ec365e9afe06a3d
Кот Анастасия <hugo.greendal@gmail.com>
2024-03-02 00:18:01 +0300

    [WIP] Project structure

---
+++ Mail.Entity/Letter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Letter.Entity
+{
+	public class Letter
+	{
+		public string ReceiverEmail { get; set; }
+		public string Subject { get; set; }
+		public string Body { get; set; }
+
+		public Letter()
+		{
+		}
+	}
+}
+
